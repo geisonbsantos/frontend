783fb452aea0980c82b101c118fa774581237953
Geison Batista dos Santos <geisonbsantos@hotmail.com>
2021-10-06 16:02:34 -0300

    06.10.21

--- src/pages/vendas/VendaComponent.vue
+++ src/pages/vendas/VendaComponent.vue
@@ -1,13 +1,28 @@
 <template>
     <DashComponent>
-        <div slot="slot-pages" class="content-pages">
-            
-            <header class="title-pages">
-                <p>Página de Vendas</p>
+        <div slot="slot-pages" class="content-info">
+            <header>
+                <h1><p>Página de Vendas</p></h1>
             </header>
-            
-        </div>
-        
+            <div class="dados">
+                <h3><p>Para gerar jogos é preciso está logado</p></h3>
+
+            </div>
+            <div class="form">
+                <form>
+                    <div class="form-group">
+                        <label for="exampleInputEmail1">Quantos jogos?</label>
+                        <input type="email" class="form-control"  placeholder="Quantos jogos?">
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleInputPassword1">Quantos números?</label>
+                        <input type="text" class="form-control"  placeholder="Quantos números?">
+                    </div>
+                    <button type="submit" class="btn btn-primary btnLogin" @click.prevent="login">Gerar</button>
+                </form>
+            </div> 
+            <div></div>  
+        </div>  
     </DashComponent>
 </template>
 
@@ -17,6 +32,17 @@
 
     export default{
         name: 'VendaComponent',
+
+        data(){
+            return{
+
+            }
+        },
+        methods: {
+            login(){
+                this.$router.push({name: 'cliente'});
+            }
+        },
 
         components: {
             DashComponent,
